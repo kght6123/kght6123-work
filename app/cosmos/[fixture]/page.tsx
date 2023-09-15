0d8791b388d1c57d7dda3dc216314391b5e85580
kght6123 <i.feeling.song+github@gmail.com>
2023-09-16 01:34:48 +0900

    initial commit.

--- app/cosmos/[fixture]/page.tsx
+++ app/cosmos/[fixture]/page.tsx
@@ -2,6 +2,8 @@ import { nextCosmosStaticParams, nextCosmosPage } from "react-cosmos-next";
 
 import * as cosmosImports from "../../../cosmos.imports";
 
+export const runtime = "edge";
+
 export const generateStaticParams = nextCosmosStaticParams(cosmosImports);
 
 export default nextCosmosPage(cosmosImports);
